d079a752b0074837470c648dd0957f10d47ffc28
michaelst15 <misuto991@gmail.com>
2022-05-01 22:16:24 +0700

    TestLogic

--- LogicTest/Task8.js
+++ LogicTest/Task8.js
@@ -1,11 +1,11 @@
 function value(){
     for(let i = 1; i <= 15; i++){
         if(i % 3 === 0 && i % 5 === 0){
-            console.log('eduwork')
-        } else if(i % 3 === 0){
-            console.log('edu')
-        } else if(i % 5 === 0){
-            console.log('work')
+            console.log('Eduwork')
+        } else if(i % 3 === 0 && i != 6){
+            console.log('Edu')
+        } else if(i % 5 === 0 && i % 2 === 1){
+            console.log('Work')
         }
         else{
             console.log(i)
