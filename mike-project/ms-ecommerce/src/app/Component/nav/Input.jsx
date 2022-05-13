ed91092a86a1ba5310f274b908db2481487178c1
michaelst15 <misuto991@gmail.com>
2022-05-13 10:11:15 +0700

    Component Input

---
+++ mike-project/ms-ecommerce/src/app/Component/nav/Input.jsx
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from "react";
+// import {useLocation} from 'react-router-dom'; 
+import axios from "axios";
+import urlApi from "../urlApi";
+import Card from "../Card";   
+
+function Input() {
+
+const [value, setValue] = useState([]);
+const [dataSearch, setDataSearch] = useState('');
+
+
+useEffect(() => {
+  axios.get(`${urlApi}/api/products`)
+  .then( response => {
+      setValue(response.data);
+      setDataSearch(response.data);
+      console.log(response.data);
+    }
+  )
+  .catch(err => {
+    return err
+  })
+}, []);
+
+const handleInput = (e) => {
+    let value = e.target.value.toLowerCase();
+    let result = [];
+    console.log(value)
+    result = value.filter((data) => {
+        return data.name.toLowerCase().includes(dataSearch.toLowerCase());
+    });
+    setDataSearch(result);
+  };
+
+
+  
+
+   return(
+       <div>
+    <div>
+    <form style={{marginTop: '14px', marginLeft: '20px'}}> 
+    <input 
+    placeholder='Cari Barang'
+    onChange={handleInput}
+    />
+    </form>
+ </div>
+       <div>
+            {value.data?.map((value, key) => {
+              return(
+                   <div key={key}>
+                    <Card
+                        img={value.image_url}
+                        h5={value.name}
+                        price={value.price} 
+                        desc={value.description}
+                        tags={value.tags?.name}
+                    />
+                   </div>
+               )
+           })}
+       </div>
+       </div>
+   )
+}
+
+export default Input;
