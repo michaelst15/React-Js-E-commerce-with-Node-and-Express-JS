e7187e8563c654f2721149b545240e5a5ce36527
michaelst15 <misuto991@gmail.com>
2022-06-01 16:17:36 +0700

    App.js

--- mike-project/ms-ecommerce/src/App.js
+++ mike-project/ms-ecommerce/src/App.js
@@ -1,14 +1,26 @@
-import Page from './app/Home/Page';
 // import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './app/Component/nav/Login';
+import Register from './app/Component/nav/Register';
+import Logout from './app/Component/nav/Logout';
+import Page from './app/Home/Page';
+import Shop from './app/Component/nav/Shop';
 
-import Product from "./app/Component/data/Product";
 
 
 function App() {
   return (
     <div className="App">
-      <Page/>
-      {/* <Product/>   */}
+      <BrowserRouter>
+        <Routes>
+          <Route exect path='/' element={<Page/>}/>
+          <Route path='/Shop' element={<Shop/>}/>
+          <Route path='/Login' element={<Login/>}/>
+          <Route path='/Register' element={<Register/>}/>
+          <Route path='/Logout' element={<Logout/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
