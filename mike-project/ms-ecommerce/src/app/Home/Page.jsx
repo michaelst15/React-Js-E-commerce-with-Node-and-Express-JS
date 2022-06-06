5a88d107f466ec6f550cc72105ba1bf0dca6071d
michaelst15 <misuto991@gmail.com>
2022-06-06 10:04:53 +0700

    Home Page

--- mike-project/ms-ecommerce/src/app/Home/Page.jsx
+++ mike-project/ms-ecommerce/src/app/Home/Page.jsx
@@ -9,7 +9,7 @@ function Page() {
     return(
      <div>
        <div className='block-one'>
-         <div className='d-flex'>          
+         <div className='label-web d-flex'>          
             <label className='mt-3 text-white'>MIKe.Store</label>                                                                       
          </div>
           <Link to='/Shop'><div style={{float: 'right', marginRight: 183, marginTop: '-25px', color: 'white'}}>Shop</div></Link>
@@ -25,7 +25,9 @@ function Page() {
                     berbagai jenis dan bentuk, yang memiliki tampilan sesuai dengan selera anda. Dan anda bisa mendapatkannya dengan menambahkannya ke keraanjang 
                     belanja. Semoga anda dapat puas dengan kualitas jam tangan yang telah di sediakan.
                   </p>
-                  <button style={{marginTop: '20px',backgroundColor: '#003CB6', border: 'none', height: '50px', width: '100px', borderRadius: '10px', color: 'white'}}>Learn</button>
+                  <button style={{marginTop: '20px',backgroundColor: '#003CB6', border: 'none', height: '50px', width: '100px', borderRadius: '10px', color: 'white'}}>
+                   <a href='#Learn' style={{textDecoration: 'none', color: 'white'}}>Learn</a> 
+                  </button>
                   </div>
                 </div>
                 <span className='image'>
@@ -39,20 +41,20 @@ function Page() {
           <div className='block-four'>
             <div className='about'>
               <div className="container bg-white" style={{marginTop: '-20px'}}>
-                <h3 className="fw-bold fst-italic mb-4">MIKe.Store</h3>
-                <p className='mb-5'>Anda dapat menemukan koleksi jam tangan yang bervariasi dan dengan gaya beragam yang anda sesuaikan 
+                <h3 className="fw-bold fst-italic mb-4" id='Learn' style={{fontFamily: 'Quattrocento, serif'}}>MIKe.Store</h3>
+                <p className='mb-5' style={{fontFamily: 'Quattrocento, serif'}}>Anda dapat menemukan koleksi jam tangan yang bervariasi dan dengan gaya beragam yang anda sesuaikan 
                 dengan selera anda sendiri, mulai dari jam tangan moder, classic, hingga sporty. Anda dengan sepuasnya dapat memilih, berbagai konsep yang telah
                 disediakan dan dengan beberapa fitur pada jam tangan, hal ini dapat mempengaruhi tingkat kepuasan anda dalam memakainya
                 . Saya telah melakukan riset dengan baik, sesuai pasar konsumen yang berlaku beberapa dekade. Dan dibawah anda dapat melihat informasi detail 
                 mengaenai beberapa jam tangan.</p>
                 <hr />                                                                               
-                <h2 className="text-center mt-5 mb-5">MIKe.Store Collection</h2>
-              <div className="container bg-white d-flex text-center">
+                <h2 className="text-center mt-5 mb-5" style={{fontFamily: 'Quattrocento, serif'}}>MIKe.Store Collection</h2>
+              <div className="card-mike-colect container bg-white d-flex text-center">
                 <div className='row'>
                   <div className='col-md-6'>
                     <Card style={{backgroundColor: '#004CE6', color: 'white', borderRadius: '20px'}}>
                     <h3 style={{marginTop: '7px'}}>G-Shock</h3>
-                    <p>G-Shock sebenarnya adalah jam tangan yang dibuat oleh Casio, namun didesain dengan model yang lebih kasual dan sporty. 
+                    <p id='paragraf-colect'>G-Shock sebenarnya adalah jam tangan yang dibuat oleh Casio, namun didesain dengan model yang lebih kasual dan sporty. 
                        Buat kamu para pria yang suka jalan-jalan jauh, menyelam ke laut, naik gunung, dan aktivitas-aktivitas berat lainnya, G-Shock adalah merk jam tangan terkenal buat kamu.
                        Selain modelnya yang keren dan bikin kamu terlihat maskulin, Jam tangan G-Shock tersedia dalam berbagai macam warna. 
                        Warna-warna seperti hitam, cokelat tua hingga biru navy bisa menjadi pilihan buat kamu yang ingin tampil macho!
@@ -62,7 +64,7 @@ function Page() {
                   <div className='col-md-6'>
                     <Card style={{backgroundColor: '#004CE6', color: 'white', borderRadius: '20px'}}>
                     <h3 style={{marginTop: '7px'}}>Casio</h3>
-                    <p>Diantara merk jam tangan lainnya, mungkin jam tangan pria Casio merupakan yang paling unik dan anti-mainstream. 
+                    <p id='paragraf-colect'>Diantara merk jam tangan lainnya, mungkin jam tangan pria Casio merupakan yang paling unik dan anti-mainstream. 
                        Karena Casio memiliki berbagai macam tipe jam tangan yang unik dan bisa menjadi pilihan buat kamu para pria yang ingin tampil beda. 
                        Casio memiliki jam tangan seperti kalkulator. Tidak hanya itu, Casio juga memiliki jam tangan video games di dalamnya, 
                        sehingga kamu bisa melihat waktu dan juga bermain games sekaligus dengan jam tangan itu!
@@ -72,12 +74,12 @@ function Page() {
                   </div>
                 </div>
               </div>
-                <div className="container bg-white d-flex text-center mt-5">
+                <div className="card-mike-colect container bg-white d-flex text-center mt-5">
                   <div className='row'>
                       <div className='col-md-6'>
                         <Card style={{backgroundColor: '#004CE6', color: 'white', borderRadius: '20px'}}>
                         <h3 style={{marginTop: '7px'}}>Fossil</h3>
-                        <p>Fossil adalah merk jam tangan buat kamu yang ingin tampil elegan dan berkelas dengan sentuhan klasik yang tentu saja menambah kesan vintage pada diri kamu. 
+                        <p id='paragraf-colect'>Fossil adalah merk jam tangan buat kamu yang ingin tampil elegan dan berkelas dengan sentuhan klasik yang tentu saja menambah kesan vintage pada diri kamu. 
                            Terutama jam tangan Fossil yang terbuat dari kulit asli berwarna cokelat. Kalau kamu memakai jam tangan Fossil yang talinya terbuat dari kulit, dijamin, 
                            seiring menuanya kulit jam tangan kamu, kamu akan terlihat lebih elegan, karena aksesori apapun yang terbuat dari kulit asli cenderung membuat kaum pria terlihat lebih manly.
                         </p>
@@ -86,7 +88,7 @@ function Page() {
                       <div className='col-md-6'>
                         <Card style={{backgroundColor: '#004CE6', color: 'white', borderRadius: '20px'}}>
                         <h3 style={{marginTop: '7px'}}>Daniel Wellington</h3>
-                        <p>Buat kamu yang ingin tampil dengan jam tangan yang lebih mencolok, ada merk jam tangan pria bernama Daniel Wellington atau yang lebih dikenal dengan sebutan DW. 
+                        <p id='paragraf-colect'>Buat kamu yang ingin tampil dengan jam tangan yang lebih mencolok, ada merk jam tangan pria bernama Daniel Wellington atau yang lebih dikenal dengan sebutan DW. 
                            Jam tangan pria keren ini merupakan salah satu jam tangan yang talinya terbuat dari bahan yang halus serta berwarna-warni, seperti kombinasi merah, putih dan biru atau satu warna saja.
                            Selain itu, angka yang digunakan pada tubuh jam tangannya menggunakan angka romawi yang tentu saja menambah kesan vintage.
                         </p>
@@ -99,12 +101,12 @@ function Page() {
         </div>
 
       <div className='block-five'>
-        <h2 className='title-Footer' style={{marginLeft: '590px', marginTop: '40px'}}>MIKe.Store</h2>
+        <h2 className='title-Footer' style={{marginLeft: '590px', marginTop: '40px', fontFamily: 'Quattrocento, serif'}}>MIKe.Store</h2>
          <div style={{marginLeft: '610px', marginTop: '23px'}}>
-            <i className="fa-brands fa-facebook mx-md-2"></i>
-            <i className="fa-brands fa-twitter mx-md-2"></i>
-            <i className="fa-brands fa-youtube mx-md-2"></i>
-            <i className="fa-brands fa-instagram mx-md-2"></i>
+            <i className="fa-brands fa-facebook mx-md-2" style={{cursor: 'pointer'}}></i>
+            <i className="fa-brands fa-twitter mx-md-2" style={{cursor: 'pointer'}}></i>
+            <i className="fa-brands fa-youtube mx-md-2" style={{cursor: 'pointer'}}></i>
+            <i className="fa-brands fa-instagram mx-md-2" style={{cursor: 'pointer'}}></i>
          </div>
          <div className='text-center mt-4'>
            <p>Powerfull by Market Swatch</p>
