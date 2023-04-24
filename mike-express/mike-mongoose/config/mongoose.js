2d3e4339e3f0c1d52692427e7e7573e6b66e7abb
michaels <95168027+michaelst15@users.noreply.github.com>
2023-04-24 22:46:24 +0700

    Delete mike-express directory

--- mike-express/mike-mongoose/config/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://mike-jobs:mike123@localhost:27017/mike-job?authSource=admin')
-const db = mongoose.connection;
-db.once('open', () => console.log('connect success'));
-db.on('error', () => console.log('not success connect'));
